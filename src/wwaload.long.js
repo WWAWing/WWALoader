1a27295b7097a3a39573b2a7687cb38eb3138127
Matsuyuki/rmn <matsuyuki.eca@gmail.com>
2017-10-23 00:32:26 +0900

    npm package

--- src/wwaload.long.js
+++
@@ -1,8 +0,0 @@
-var loader_conf;
-(function (loader_conf) {
-    ;
-    loader_conf.conf = {
-        is_worker: false
-    };
-})(loader_conf || (loader_conf = {}));
-//# sourceMappingURL=wwaload.long.js.map
